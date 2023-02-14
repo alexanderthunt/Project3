cdd6c4c4c4cac83576df47ec1ca1897aadea5d20
twcl774 <tristanwright318@revature.net>
2023-02-14 00:36:09 -0500

    ADD new variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,6 @@ variable "spring_password" {}
 variable "spring_username" {}
 variable "email_username" {}
 variable "email_password" {}
+variable "email_address" {}
+variable "load_balancer" {}
+variable "alert_manager" {}
